1819200b34dd949edfdde7be449a60ef6cc8b76f
alex <darkeitor6@outlook.com>
2024-07-30 17:49:50 -0500

    cors

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 import express from "express"
 import db from "./db.js"
-
+import cors from "cors"
 const PORT = process.env.PORT
 const app = express()
 
 import textRouter from "./routes/texts.js"
-
+app.use(cors())
 app.use(express.json())
 
 app.get("/api", (req, res) => {
